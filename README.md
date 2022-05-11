c235741ff53a3f69f35997dc87ada6743661624c
Sachin Acharya <acharyaraj71+github_project@gmail.com>
2022-05-11 22:16:59 +0545

    READMEMD Updated

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # fetch-global-package
+___________________________________________
+### Description
+This is a JavaScript Library that makes use of fetch web api and provide resolve data globally or to certain scope.
+
+### Example
+1. HTML File
+````html
+...
+<body>
+    ...
+    <!--
+        Note:
+        index.js is users JavaScript file
+        is type must be specified as module inside <script> tag
+    -->
+    <script type='module' src='index.js'></script>
+    ...
+</body>
+...
+````
+2. Inside JavaScript File (index.js)
+````javascript
+    import Fetch from "/path/fetch-global.js"; // Importing or linking library with javascript file
+    const fetch_ = new Fetch(url: string, options: object) // making instance of Fetch class
+    await fetch_.start() // Starting XHR process
+
+    fetch_.[items] // Geting Results back
+````
+[Get More Information about the 'options' parameter and .[items]](###Parameters and More)
---
+++ fetch-global.js
@@ -0,0 +1,38 @@
+/**
+ * @typedef HeadersAPI
+ * @property {"application/json" | "application/x-www-form-urlencoded"} ContentType
+ */
+
+/**
+ * @typedef FetchAPI
+ * @property {"GET" | "POST" | "PUT" | "DELETE"} method *GET, POST, PUT, DELETE, etc.
+ * @property {"no-cors" | "cors" | "same-origin"} mode no-cors, *cors, same-origin
+ * @property {"default" | "no-cache" | "reload" | "force-cache" | "only-if-cached"} cache *default, no-cache, reload, force-cache, only-if-cached
+ * @property {"include" | "same-origin" | "omit"} credentials include, *same-origin, omit
+ * @property {HeadersAPI} headers
+ * @property {"manual" | "follow" | "error"} redirect manual, *follow, error
+ * @property {*} body cannot be used with GET method & body data type must match "Content-Type" header (content to be send)
+ * @property {"no-referrer" | "no-referrer-when-downgrade" | "origin" | "origin-when-cross-origin" | "same-origin" | "strict-origin" | "strict-origin-when-cross-origin" | "unsafe-url"} referrerPolicy no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+ */
+
+class Fetch {
+    /**
+     * This constructor functions initialize the method Start for processing
+     * @param {string} url 
+     * @param {FetchAPI} options
+     */
+    constructor(url, options) {
+        this.url = url
+        this.options = options
+        this.error = null
+    }
+    async start() {
+        await fetch(this.url, this.options)
+        // .then(results => this.stat = results)
+        .then(results => results.json())
+        .then(results => this.data = results)
+        .catch(err => this.error = err)
+    }
+}
+
+export default Fetch
