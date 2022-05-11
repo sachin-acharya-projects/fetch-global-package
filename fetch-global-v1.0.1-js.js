5ba497d58c2c53844acc07e5ddb165006c50c005
Sachin Acharya <acharyaraj71+github_project@gmail.com>
2022-05-12 00:12:05 +0545

    READMEMD Updated

--- fetch-global-v1.0.1-js.js
+++ fetch-global-v1.0.1-js.js
@@ -15,7 +15,7 @@
  * @property {"no-referrer" | "no-referrer-when-downgrade" | "origin" | "origin-when-cross-origin" | "same-origin" | "strict-origin" | "strict-origin-when-cross-origin" | "unsafe-url"} referrerPolicy no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
  */
 
- class Fetch {
+class Fetch {
     /**
      * This constructor functions initialize the method Start for processing
      * @param {string} url 
